4b626aa46f97628ec0afd8ff6415873e48d72928
Philo van Kemenade <philovankemenade@gmail.com>
2022-03-24 22:09:36 +0100

    use blue text for link

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-column justify-between items-center tc vh-100">
     <h1>{{ title }}</h1>
-    <div class="pointer" @click="onLandingDrag">{{ slotValue }}</div>
+    <div class="pointer blue" @click="onLandingDrag">{{ slotValue }}</div>
     <CardItem>{{ cardValue }}</CardItem>
     <TheFooter>
       Empathy Spectrum is a result of the Tolerant Futures research project.
