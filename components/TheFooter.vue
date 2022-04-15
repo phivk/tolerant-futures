414be3383d575a6e7aaa1d2dac8895296d20aa25
Philo van Kemenade <philovankemenade@gmail.com>
2022-04-15 23:34:11 +0200

    simplify footer
    removed unused backButton

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,31 +1,5 @@
 <template>
   <footer class="w-100 pa3">
     <slot></slot>
-    <div v-if="shouldShowBack" class="ma3">
-      <a class="bg-white purple" href="" @click.prevent="onBackClick"
-        >Go Back</a
-      >
-    </div>
   </footer>
 </template>
-
-<script>
-export default {
-  computed: {
-    shouldShowBack() {
-      return this.$router.currentRoute.path !== '/'
-    },
-  },
-  methods: {
-    onBackClick() {
-      if (this.$nuxt.context.from) {
-        this.$router.back()
-      } else {
-        this.$router.push({
-          path: '/',
-        })
-      }
-    },
-  },
-}
-</script>
