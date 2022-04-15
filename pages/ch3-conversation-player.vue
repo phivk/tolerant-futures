4e3da550f799c43899b6482b1b8e6441881f8dce
Philo van Kemenade <philovankemenade@gmail.com>
2022-04-15 14:27:54 +0200

    rename pages

---
+++ pages/ch3-conversation-player.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-column justify-between items-center tc vh-100">
+    <h1>{{ title }}</h1>
+    <SpectrumGame :turns="playTurns" :next-path="nextPath" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Play',
+      playTurns: [
+        {
+          object: 'Boudica',
+          spectrum: ['Submissive', 'Dominant'],
+        },
+        {
+          object: 'Roman Empire',
+          spectrum: ['Proud', 'Modest'],
+        },
+        {
+          object: "Hadrian's Wall",
+          spectrum: ['Generous', 'Greedy'],
+        },
+      ],
+      nextPath: '/future',
+    }
+  },
+}
+</script>
