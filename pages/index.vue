d3abe83d8cc948231dcb9b597b3feccce095e0ea
m <matus.solcany@protonmail.com>
2022-04-20 22:17:26 +0200

    style content wrapper

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="landing-page vh-100">
-    <div>
-    <h1>{{ title }}</h1>
-    <p>{{ descriptionText }}</p>
-    <NuxtLink class="bg-white purple" :to="nextPath">
-      {{ nextButtonText }}
-    </NuxtLink>
+    <div class="wrapper">
+      <h1>{{ title }}</h1>
+      <p>{{ descriptionText }}</p>
+      <NuxtLink class="bg-white purple" :to="nextPath">
+        {{ nextButtonText }}
+      </NuxtLink>
+      <NuxtLink class="bg-white purple" :to="aboutProjectPath">
+        {{ aboutProjectText }}
+      </NuxtLink>      
     </div>
   </div>
 </template>
@@ -18,6 +21,9 @@ export default {
       nextButtonText: 'Begin experience',
       descriptionText: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.',
       nextPath: '/ch1',
+      aboutProjectPath: '/about',
+      aboutProjectText: 'read more',
+
     }
   },
 }
@@ -25,11 +31,27 @@ export default {
 
 <style scoped lang="scss">
   div.landing-page {
-    background-image: url('@/static/image/landing_page_background.jpg');
-    object-fit: cover;
+      display: block;
+      padding: $offset-4;    
+      background-image: url('@/static/image/landing_page_background.jpg');
+      object-fit: cover;
 
-    h1 {
-      margin: 0;
+    .wrapper {
+      width: 20%;
+      min-width: 300px;      
+      background-color: rgba(255,255,255,0.5); 
+      border-radius: $border-radius-2;
+      padding: $offset-4;
+
+      h1, p, a {
+        display: block;
+      }
+
+      p {
+        font-family: $text-font;
+        font-size: $f-5;
+        margin: $offset-4 0;
+      }
     }
   } 
 </style>
