fbe7b4b89a40c086f58781d5029e4faa8309dd80
m <matus.solcany@protonmail.com>
2022-04-26 18:59:28 +0200

    style form wrapper

--- components/ModalPlayerFeedback.vue
+++ components/ModalPlayerFeedback.vue
@@ -1,7 +1,9 @@
-<template>
-  <form>
-    <input type="text" id="fname" name="fname"><br><br>
-  </form>
+<template> 
+  <div>
+    <form>
+      <input type="text" id="fname" name="fname"><br><br>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -13,8 +15,40 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  form {
-    position: absolute;
-    z-index: $z-5;
+
+div {
+  width: 60%;
+  height: 600px;
+  position: absolute;
+  z-index: $z-5;
+  border-radius: $border-radius-2;
+  padding: $offset-3;  
+  background-color: rgba(255, 255, 255, .5);
+  @supports ((-webkit-backdrop-filter: blur(10px)) or (backdrop-filter: blur(10px))) {
+    background-color: rgba(255, 255, 255, .3);
+    -webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
   }
+
+  form {
+    width: 100%;
+    height: 100%;
+
+    input {
+      background: none;
+      outline: none;
+      width: 100%;
+      appearance: none; 
+      -moz-appearance: none; 
+      -webkit-appearance: none; 
+      border: none;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+
+
+}
 </style>
