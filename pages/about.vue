d1dd65fc53dfa453c040892ee852a16a7b7014fd
m <matus.solcany@protonmail.com>
2022-05-10 08:14:27 +0200

    about page styles

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="flex flex-column justify-between items-center tc vh-100">
-    <h1>{{ title }}</h1>
+    <header>
+      <ButtonExitGame/>
+    </header>
     <section class="measure">
+      <h1><NuxtLink to="/">{{ title }}</NuxtLink></h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo nobis
         corporis iste omnis quaerat aut, aperiam hic! Ipsum, dolorum. Aliquid
         sit excepturi accusamus pariatur similique. Eveniet ab molestias
         repellat officiis.
       </p>
+      <h2> Research </h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur, adipisicing elit. Iste
         doloribus facilis voluptatem nihil quas excepturi magni provident
@@ -25,8 +29,25 @@
 export default {
   data() {
     return {
-      title: 'Tolerant Futures - the research',
+      title: 'Emapthy Spectrum',
+
     }
   },
 }
 </script>
+
+<style scoped lang="scss">
+  
+  header {
+    width: 100%;
+
+    a {
+      padding-top: 200px;
+    }
+    height: 150px;
+  }
+
+  h1,h2,p {
+    color: $white-color;
+   }
+</style>
