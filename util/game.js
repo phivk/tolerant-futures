4e340c52779f740c817ee6579698cd91e63dacbb
Philo van Kemenade <philovankemenade@gmail.com>
2022-05-15 22:48:25 +0200

    remove deprecated functions

--- util/game.js
+++ util/game.js
@@ -34,36 +34,6 @@ export const getRandomConceptSelfOther = (priorInputs) => {
   }
 }
 
-export const getRandomTurnPastPresent = () => {
-  const conceptPair = getRandomConceptPastPresent()
-  const spectrum = getRandomSpectrum()
-  return {
-    concept: conceptPair.past.name,
-    caption: conceptPair.past.caption,
-    hint: conceptPair.past.hint,
-    conceptPresent: conceptPair.present.name,
-    captionPresent: conceptPair.present.caption,
-    spectrumLeft: spectrum[0],
-    spectrumRight: spectrum[1],
-  }
-}
-
-export const getRandomTurnSelfOther = (priorInputs) => {
-  const conceptPair = getRandomConceptSelfOther(priorInputs)
-
-  return {
-    concept: conceptPair.self.name,
-    caption: conceptPair.self.caption,
-    hint: conceptPair.self.hint,
-    conceptOther: conceptPair.other.concept,
-    valueOther: conceptPair.other.value,
-    captionOther: `Where do you think the other visitor placed ${conceptPair.other.concept} on this spectrum?`,
-    spectrumLeft: conceptPair.other.spectrumLeft,
-    spectrumRight: conceptPair.other.spectrumRight,
-    otherUser: conceptPair.other.user,
-  }
-}
-
 export const getRandomTurnsPastPresent = (n) => {
   // pick n concepts randomly from list
   // then augment each concept with spectrum to build turn
