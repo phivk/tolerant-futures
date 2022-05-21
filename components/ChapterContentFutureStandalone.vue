94c881da2d4286fa01970af6563942627c1da992
Philo van Kemenade <philovankemenade@gmail.com>
2022-05-21 16:04:22 +0200

    make ChapterContentFutureStandalone state based

--- components/ChapterContentFutureStandalone.vue
+++ components/ChapterContentFutureStandalone.vue
@@ -1,10 +1,16 @@
 <template>
   <ChapterWrapperFuture
     :title="title"
-    :paragraph="paragraph"
-    button-text="Back to start"
-    next-path="/"
-  />
+    :paragraph="currentState.caption"
+    :show-button="false"
+  >
+    <ButtonPrimary
+      v-if="currentState.buttonPrimary"
+      @buttonClicked="currentState.buttonPrimary.handler"
+    >
+      {{ currentState.buttonPrimary.text }}
+    </ButtonPrimary>
+  </ChapterWrapperFuture>
 </template>
 
 <script>
@@ -12,9 +18,41 @@ export default {
   data() {
     return {
       title: 'Chapter 4: The Future',
-      paragraph:
-        "As part of this installation, you'll find a set of physical cards. We invite you to pick one and take it with you. What meaning do you attach to this card? And how would someone else see it? Feel free to use this card as a keepsake or a conversation starter whenever the moment is right.",
+      currentStateKey: 'pickupCard',
     }
+  },
+  computed: {
+    states() {
+      return {
+        pickupCard: {
+          caption:
+            "As part of this installation, you'll find a set of physical cards. We invite you to pick one and take it with you. What meaning do you attach to this card? And how would someone else see it? Feel free to use this card as a keepsake or a conversation starter whenever the moment is right.",
+          buttonPrimary: {
+            text: 'Continue',
+            handler: this.onContinue,
+          },
+        },
+        endState: {
+          caption:
+            'Thank you for participating. May your future be bright and tolerant!',
+          buttonPrimary: {
+            text: 'Back to start',
+            handler: this.onBackToStart,
+          },
+        },
+      }
+    },
+    currentState() {
+      return this.states[this.currentStateKey]
+    },
+  },
+  methods: {
+    onContinue() {
+      this.currentStateKey = 'endState'
+    },
+    onBackToStart() {
+      this.$router.push('/')
+    },
   },
 }
 </script>
