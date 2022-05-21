200dc57c9f74a6d9ecef0b4adfc27723b8c11e22
Philo van Kemenade <philovankemenade@gmail.com>
2022-05-21 15:28:30 +0200

    componentise ChapterContentFutureBrowser and ChapterContentFutureStandalone

---
+++ components/ChapterContentFutureStandalone.vue
@@ -0,0 +1,20 @@
+<template>
+  <ChapterWrapperFuture
+    :title="title"
+    :paragraph="paragraph"
+    button-text="Back to start"
+    next-path="/"
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Chapter 4: The Future',
+      paragraph:
+        "As part of this installation, you'll find a set of physical cards. We invite you to pick one and take it with you. What meaning do you attach to this card? And how would someone else see it? Feel free to use this card as a keepsake or a conversation starter whenever the moment is right.",
+    }
+  },
+}
+</script>
--- pages/ch4/play.vue
+++ pages/ch4/play.vue
@@ -1,124 +1,18 @@
 <template>
   <StandaloneConditionalDisplay>
     <template #standalone>
-      <ChapterWrapperFuture
-        :title="title"
-        :paragraph="paragraphStandalone"
-        button-text="Back to start"
-        next-path="/"
-      />
+      <ChapterContentFutureStandalone />
     </template>
     <template #browser>
-      <ChapterWrapperFuture
-        :title="title"
-        :paragraph="currentState.caption"
-        :show-button="false"
-      >
-        <div class="mv4 tc">
-          <component
-            :is="currentState.component"
-            class="mv3"
-            v-bind="currentState.dynamicProps"
-            @feedbackSubmitted="onFeedbackSubmitted"
-            @feedbackSkipped="onFeedbackSkipped"
-          />
-          <ButtonPrimary
-            v-if="currentState.buttonPrimary"
-            @buttonClicked="currentState.buttonPrimary.handler"
-          >
-            {{ currentState.buttonPrimary.text }}
-          </ButtonPrimary>
-          <ButtonSecondary
-            v-if="currentState.buttonSecondary"
-            @buttonClicked="currentState.buttonSecondary.handler"
-          >
-            {{ currentState.buttonSecondary.text }}
-          </ButtonSecondary>
-        </div>
-      </ChapterWrapperFuture>
+      <ChapterContentFutureBrowser />
     </template>
   </StandaloneConditionalDisplay>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      title: 'Chapter 4: The Future',
-      paragraphStandalone:
-        "As part of this installation, you'll find a set of physical cards. We invite you to pick one and take it with you. What meaning do you attach to this card? And how would someone else see it? Feel free to use this card as a keepsake or a conversation starter whenever the moment is right.",
-      feedback: '',
-      currentStateKey: 'inputFeedback',
-    }
-  },
-  computed: {
-    states() {
-      return {
-        inputFeedback: {
-          caption:
-            'We invite you to take a moment and reflect: When does a tolerant future unfold for you?',
-          component: 'FeedbackInput',
-          dynamicProps: {
-            inputPlaceholderText: 'A tolerant future unfolds for me, when ...',
-          },
-        },
-        displayFeedback: {
-          caption:
-            'Thank you. Sharing your thought with someone else can be a great first step in making it real. Why not start by sending out your message to a friend?',
-          component: 'ModalTextRevealer',
-          dynamicProps: {
-            text: this.feedback,
-            isHidden: false,
-          },
-          buttonPrimary: {
-            text: 'Email a friend',
-            handler: this.onEmailClick,
-          },
-          buttonSecondary: {
-            text: 'Skip',
-            handler: this.onEmailSkip,
-          },
-        },
-        endState: {
-          caption:
-            'Thank you for participating. May your future be bright and tolerant!',
-          buttonPrimary: {
-            text: 'Back to start',
-            handler: this.onBackToStart,
-          },
-        },
-      }
-    },
-    currentState() {
-      return this.states[this.currentStateKey]
-    },
-    mailtoURL() {
-      return `mailto:${encodeURI(
-        `add@your-friends-email.here?subject=A tolerant future ...&body=${this.feedback}`
-      )}`
-    },
-  },
   mounted() {
     this.$store.commit('setCurrentChapter', 'ch4')
-  },
-  methods: {
-    onFeedbackSubmitted(feedbackText) {
-      this.feedback = feedbackText
-      this.currentStateKey = 'displayFeedback'
-    },
-    onFeedbackSkipped() {
-      this.currentStateKey = 'endState'
-    },
-    onEmailClick() {
-      window.open(this.mailtoURL)
-      this.currentStateKey = 'endState'
-    },
-    onEmailSkip() {
-      this.currentStateKey = 'endState'
-    },
-    onBackToStart() {
-      this.$router.push('/')
-    },
   },
 }
 </script>
