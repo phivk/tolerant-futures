fae86a421f66028e35a6e239680eb720d7a963e4
Philo van Kemenade <philovankemenade@gmail.com>
2022-05-22 10:57:03 +0200

    implement idle view screen with countdown timer

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,16 @@
     <Nuxt />
     <div
       v-if="isAppIdle"
-      class="w-100 vh-100 bg-white-50 absolute top-0 z-999 flex justify-center items-center"
+      class="w-100 vh-100 bg-white-90 absolute top-0 z-999 flex justify-center items-center"
     >
-      Idle View!
+      <ModalTextRevealer
+        class="tc"
+        text="Are you still there?"
+        :is-hidden="false"
+      >
+        <p class="mv3">touch to continue</p>
+        <p class="mv3">restarting in {{ timerCount }}</p>
+      </ModalTextRevealer>
     </div>
   </div>
 </template>
@@ -14,15 +21,54 @@ export default {
   data() {
     return {
       isAppIdle: false,
+      timerEnabled: false,
+      timerCountStart: 5,
+      timerCount: this.timerCountStart,
     }
   },
   onIdle() {
     console.log('ZZZ')
     this.isAppIdle = true
+    this.timerEnabled = true
   },
   onActive() {
     console.log('Hello')
+    this.resetTimer()
     this.isAppIdle = false
+  },
+  watch: {
+    timerEnabled(value) {
+      console.log('timerEnabled value', value)
+      if (value) {
+        setTimeout(() => {
+          this.timerCount--
+        }, 1000)
+      }
+    },
+    timerCount: {
+      handler(value) {
+        console.log('timerCount value', value)
+        if (value > 0 && this.timerEnabled) {
+          setTimeout(() => {
+            this.timerCount--
+          }, 1000)
+        } else if (value <= 0 && this.timerEnabled) {
+          this.onTimerEnded()
+        }
+      },
+      // immediate: true, // This ensures the watcher is triggered upon creation
+    },
+  },
+  methods: {
+    resetTimer() {
+      this.timerEnabled = false
+      this.timerCount = this.timerCountStart
+    },
+    onTimerEnded() {
+      this.resetTimer()
+      this.isAppIdle = false
+      this.$router.push('/')
+    },
   },
 }
 </script>
